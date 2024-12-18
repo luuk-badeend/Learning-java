8fd3de9f575071578de3eff0099da3e298136243
luuk-badeend <lbattjes25@gmail.com>
2024-12-18 11:12:47 +0100

    added user input for username and password

--- User.java
+++ User.java
@@ -3,24 +3,31 @@ import java.io.Console;
 import java.security.MessageDigest;
 
 public class User {
+    String username;
+    String hashedPassword;
+
     public User() {
+        username = Helper.getString("Enter username: ");
+        hashedPassword = getHashedPassword();
+
+        System.out.println(username + "\n" + hashedPassword);
+    }
+
+    private String getHashedPassword() {
         try {
 
             // Create console instance
             Console console = System.console();
 
-            if (console == null) {
-                System.out.println("No console available");
-                // Use return statement to exit the current method if console isn't found
-                return;
-            }
+            if (console == null)
+                throw new Error("No console available");
 
             // Get password from cli with readpassword method to not echo charachters while
             // typing
             // Output from console.readPassword must be saved in charachter array
             char[] passwordArray = console.readPassword("Enter password: ");
 
-            // use String() to conver charachter array to string
+            // use String() to convert charachter array to string
             String password = new String(passwordArray);
 
             // Hashes password but don't understand:
@@ -34,7 +41,7 @@ public class User {
                 sb.append(String.format("%02x", b));
             }
 
-            System.out.println(sb.toString());
+            return sb.toString();
         } catch (Exception e) {
             throw new Error(e.getMessage());
         }
